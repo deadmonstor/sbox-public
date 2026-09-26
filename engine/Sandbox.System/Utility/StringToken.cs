ded6491cf3a19e1b9c4820046565819faab73bf3
Josh Mobley <8387317+deadmonstor@users.noreply.github.com>
2026-09-26 17:11:17 +0100

    Fix StringToken reverse lookup casing

--- engine/Sandbox.System/Utility/StringToken.cs
+++ engine/Sandbox.System/Utility/StringToken.cs
@@ -41,7 +41,7 @@ public struct StringToken : IEquatable<StringToken>
 		if ( value.Length == 0 ) return 0;
 
 		var token = value.MurmurHash2( true );
-		CacheReverse[token] = value;
+		CacheReverse[token] = value.ToLowerInvariant();
 		return token;
 	}
 
